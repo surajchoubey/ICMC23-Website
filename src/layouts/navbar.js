c30943dcab7ff8fa96c441f88c39e8d509121c72
Suraj Choubey <srjchoubey2@gmail.com>
2022-01-03 13:30:00 +0530

    fixed navbar content-end

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -6,38 +6,40 @@ function MyNavbar() {
 
 	return (
     <div>
-		<nav class="navbar navbar-light bg-secondary px-3 py-1 navbar-expand-lg">
-			<a class="navbar-brand text-white" href="/ICMC23-Website/">
-			<img src={NavLogo} width="30" height="30" class="d-inline-block align-top p-1" alt="" />
-				ICMC Conference 2023
-			</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class='collapse navbar-collapse' id="navbarSupportedContent">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/'>Home</Link>
-					</li>
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/about'>About</Link>
-					</li>
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/speakers'>Speakers</Link>
-					</li>
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/registration'>Registration</Link>
-					</li>
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/pasteditions'>Past Editions</Link>
-					</li>
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/publications'>Publications</Link>
-					</li>
-					<li class="nav-item">
-						<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/contact'>Contact Us</Link>
-					</li>
-				</ul>
+		<nav class="navbar navbar-light bg-secondary px-3 py-1 navbar-expand-lg sticky-top">
+			<div class="container-fluid">
+				<a class="navbar-brand text-white" href="/ICMC23-Website/">
+				<img src={NavLogo} width="30" height="30" class="d-inline-block align-top p-1" alt="" />
+					ICMC Conference 2023
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class='collapse navbar-collapse justify-content-end' id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/'>Home</Link>
+						</li>
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/about'>About</Link>
+						</li>
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/speakers'>Speakers</Link>
+						</li>
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/registration'>Registration</Link>
+						</li>
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/pasteditions'>Past Editions</Link>
+						</li>
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/publications'>Publications</Link>
+						</li>
+						<li class="nav-item">
+							<Link className='nav-link text-white' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/contact'>Contact Us</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
     </div>
