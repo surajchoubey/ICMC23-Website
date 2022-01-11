6534a0ad0c6b826dc974a3524f5edfb5fd615112
Shruti Rastogi <shruti.kuku@gmail.com>
2022-01-11 15:34:36 +0530

    added pages

--- src/pages/callforpapers/tracks.js
+++ src/pages/callforpapers/tracks.js
@@ -1,9 +1,115 @@
 import React from "react";
+import '../../App.css';
 
 function Tracks() {
     return(
-        <div>
-            This is the CallForPapers/Tracks Page.
+        <div style={{marginLeft:"60px", marginTop: "40px", marginRight: '60px'}}>
+        <div className="titleStyle1">
+            Contribution
+        </div>
+        <div className="titleStyle2">
+            Tracks
+        </div>
+        <div className = "tracksTitle">
+            mathematics
+        </div>
+        <div style={{display:'flex', flexWrap: 'wrap', justifyContent: "space-between", marginTop: "120px"}}>
+            <div className="tracksStyle">
+                NUMERICAL ANALYSIS
+            </div>
+            <div className="tracksStyle">
+                APPROXIMATION THEORY
+            </div>
+            <div className="tracksStyle">
+                LINEAR ALGEBRA
+            </div>
+            <div className="tracksStyle">
+                DIFFERENTIAL EQUATIONS
+            </div>
+            <div className="tracksStyle">
+                COMPUTATIONAL NUMBER THEORY
+            </div>
+            <div className="tracksStyle">
+                ANALYSIS
+            </div>
+            <div className="tracksStyle">
+                OPERATIONS RESEARCH
+            </div>
+            <div className="tracksStyle">
+                PROBABILITY AND STATISTICS
+            </div>
+            <div className="tracksStyle">
+                MATHEMATICAL MODELING AND SIMULATION
+            </div>
+            <div className="tracksStyle">
+                GEOMETRY (DISCRETE, ALGEBRAIC, FINITE)
+            </div>
+            <div className="tracksStyle">
+                APPLICATIONS OF FUZZY SET THEORY
+            </div>
+            <div className="tracksStyle">
+                DISCRETE MATHEMATICS
+            </div>
+            <div className="tracksStyle">
+                GAME THEORY
+            </div>
+            <div className="tracksStyle">
+                COMPUTATIONAL FLUID MECHANICS
+            </div>
+            <div className="tracksStyle">
+                INFORMATION THEORY
+            </div>
+        </div>
+        <div className = "tracksTitle">
+            computing
+        </div>
+        <div style={{display:'flex', flexWrap: 'wrap', justifyContent: "space-between", marginTop: "120px"}}>
+            <div className="tracksStyle">
+                WIRELESS NETWORKS
+            </div>
+            <div className="tracksStyle">
+                NETWORK SECURITY
+            </div>
+            <div className="tracksStyle">
+                DIGITAL IMAGE PROCESSING
+            </div>
+            <div className="tracksStyle">
+                DIGITAL WATERMARKING
+            </div>
+            <div className="tracksStyle">
+                CRYPTOLOGY AND CYBER SECURITY
+            </div>
+            <div className="tracksStyle">
+                MOBILE, DISTRIBUTED, AND PARALLEL COMPUTING
+            </div>
+            <div className="tracksStyle">
+                MACHINE AND DEEP LEARNING
+            </div>
+            <div className="tracksStyle">
+                GREEN COMPUTING
+            </div>
+            <div className="tracksStyle">
+                BIG DATA
+            </div>
+            <div className="tracksStyle">
+                CLOUD, FOG, AND EDGE COMPUTING
+            </div>
+            <div className="tracksStyle">
+                BIOINFORMATICS
+            </div>
+            <div className="tracksStyle">
+                QUANTUM COMPUTING
+            </div>
+            <div className="tracksStyle">
+                INTERNET OF THINGS
+            </div>
+            <div className="tracksStyle">
+                BLOCKCHAIN
+            </div>
+            <div className="tracksStyle">
+                DEW COMPUTING
+            </div>
+        </div>
         </div>
     );
 }
--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -1,9 +1,76 @@
 import React from "react";
+import speaker1 from '../images/speaker1.jpg';
+import speaker2 from '../images/speaker2.jpg';
 
 function Speakers() {
     return(
-        <div>
-            This is the Speakers page.
+        <div style={{marginLeft:"60px", marginTop: "40px", marginRight:"60px"}}>
+        <div className="titleStyle1">
+            Our
+        </div>
+        <div className="titleStyle2">
+            Speakers
+        </div>
+        <div style={{height: "350px", marginTop: "40px", 
+                    background: "#FBFBFB", display: "flex", flexDirection:"row", justifyContent: 'space-between'}}>
+            <img src={speaker1} style={{height: "350px", marginRight: "20px"}}/>
+            <div style={{height: "350px", marginRight: "0px", width: "260px"}}>
+                <div className="speakerName">
+                Jiawei Han
+                </div>
+                <div className="speakerText" style={{marginTop: "45px"}}>
+                Department of Computer Science <br/>
+                Univ. of Illinois at Urbana-Champaign
+                </div>
+                <div className="boxStyle" style={{backgroundColor:"white", marginTop: "40px"}}>
+                    <a href='https://google.com/' style={{color:"black", textDecoration:'none'}} target="_blank">VIEW MORE &#160; &#x2192;</a>
+                </div>
+            </div>
+            <div style={{marginRight: "40px", width: "400px"}}>
+                <div style={{position: "relative", height: "28px", marginTop: "32px", fontFamily: "Roboto",
+                            fontWeight: "300", fontSize: "21px",lineHeight: "28px",color: "#BDBDBD", textAlign:"left"}}>
+                    Current Research
+                </div>
+                <div className="speakerText" style={{marginTop: "15px", flexWrap: "wrap"}}>
+                §  StructNet: Constructing and Mining Structure-Rich Information Networks for Scientific Research (NSF/IIS) <br/>
+                §  Taming Big Networks via Embedding (NSF/IIS-BIGDATA) <br/>
+                §  Mining and Leveraging Knowledge Hypercubes for Complex Applications (NSF-IIS) <br/>
+                §  NSF AI Institute for Molecular Discovery, Synthetic Strategy, and Manufacturing: Molecule Maker Lab Institute <br/>
+                §  DARPA/KAIROS: “RESIN: Reasoning about Event Schemas for Induction of kNowledge” <br/>
+                §  DARPA/INCAS: “Analytics of Information Influence: Effect Characterization” <br/>
+                </div>
+            </div>
+        </div>
+        <div style={{height: "350px", marginTop: "40px", 
+                    background: "#FBFBFB", display: "flex", flexDirection:"row", justifyContent: 'space-between'}}>
+            <img src={speaker2} style={{height: "350px", marginRight: "20px"}}/>
+            <div style={{height: "350px", marginRight: "0px", width: "260px"}}>
+                <div className="speakerName">
+                Ellena Ferrari
+                </div>
+                <div className="speakerText" style={{marginTop: "45px"}}>
+                Department of Computer Science <br/>
+                Univ. of Illinois at Urbana-Champaign
+                </div>
+                <div className="boxStyle" style={{backgroundColor:"white", color:"black", marginTop: "40px"}}>
+                    <a href='https://google.com/' style={{color:"black", textDecoration:'none'}} target="_blank">VIEW MORE &#160; &#x2192;</a>
+                </div>
+            </div>
+            <div style={{marginRight: "40px", width: "400px"}}>
+                <div style={{position: "relative", height: "28px", marginTop: "32px", fontFamily: "Roboto",
+                            fontWeight: "300", fontSize: "21px",lineHeight: "28px",color: "#BDBDBD", textAlign:"left"}}>
+                    Current Research
+                </div>
+                <div className="speakerText" style={{marginTop: "15px", flexWrap: "wrap"}}>
+                §  StructNet: Constructing and Mining Structure-Rich Information Networks for Scientific Research (NSF/IIS) <br/>
+                §  Taming Big Networks via Embedding (NSF/IIS-BIGDATA) <br/>
+                §  Mining and Leveraging Knowledge Hypercubes for Complex Applications (NSF-IIS) <br/>
+                §  NSF AI Institute for Molecular Discovery, Synthetic Strategy, and Manufacturing: Molecule Maker Lab Institute <br/>
+                §  DARPA/KAIROS: “RESIN: Reasoning about Event Schemas for Induction of kNowledge” <br/>
+                §  DARPA/INCAS: “Analytics of Information Influence: Effect Characterization” <br/>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
