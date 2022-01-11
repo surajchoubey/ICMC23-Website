c4d789b0f474a9d77edf8a2614be95d5c34521ca
Shruti Rastogi <shruti.kuku@gmail.com>
2022-01-11 15:35:16 +0530

    added pages

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -18,7 +18,7 @@ function Speakers() {
                 <div className="speakerName">
                 Jiawei Han
                 </div>
-                <div className="speakerText" style={{marginTop: "45px"}}>
+                <div className="speakerText" style={{marginTop: "25px"}}>
                 Department of Computer Science <br/>
                 Univ. of Illinois at Urbana-Champaign
                 </div>
@@ -37,37 +37,39 @@ function Speakers() {
                 §  Mining and Leveraging Knowledge Hypercubes for Complex Applications (NSF-IIS) <br/>
                 §  NSF AI Institute for Molecular Discovery, Synthetic Strategy, and Manufacturing: Molecule Maker Lab Institute <br/>
                 §  DARPA/KAIROS: “RESIN: Reasoning about Event Schemas for Induction of kNowledge” <br/>
-                §  DARPA/INCAS: “Analytics of Information Influence: Effect Characterization” <br/>
+                §  DARPA/INCAS: “Analytics of Information Influence: Effect Characterization”
                 </div>
             </div>
         </div>
         <div style={{height: "350px", marginTop: "40px", 
                     background: "#FBFBFB", display: "flex", flexDirection:"row", justifyContent: 'space-between'}}>
             <img src={speaker2} style={{height: "350px", marginRight: "20px"}}/>
-            <div style={{height: "350px", marginRight: "0px", width: "260px"}}>
+            <div style={{marginRight: "0px", width: "260px"}}>
                 <div className="speakerName">
                 Ellena Ferrari
                 </div>
-                <div className="speakerText" style={{marginTop: "45px"}}>
-                Department of Computer Science <br/>
-                Univ. of Illinois at Urbana-Champaign
+                <div className="speakerText" style={{marginTop: "25px"}}>
+                Professor of Database Management Systems <br/>
+                Director, DiSTA STRICT SociaLab <br/>
+                <br/>
+                University of Insubria <br/>
+                Department of Theoretical and Applied Science
                 </div>
-                <div className="boxStyle" style={{backgroundColor:"white", color:"black", marginTop: "40px"}}>
+                <div className="boxStyle" style={{backgroundColor:"white", color:"black", marginTop: "125px"}}>
                     <a href='https://google.com/' style={{color:"black", textDecoration:'none'}} target="_blank">VIEW MORE &#160; &#x2192;</a>
                 </div>
             </div>
             <div style={{marginRight: "40px", width: "400px"}}>
                 <div style={{position: "relative", height: "28px", marginTop: "32px", fontFamily: "Roboto",
                             fontWeight: "300", fontSize: "21px",lineHeight: "28px",color: "#BDBDBD", textAlign:"left"}}>
-                    Current Research
+                    Recent Publications
                 </div>
                 <div className="speakerText" style={{marginTop: "15px", flexWrap: "wrap"}}>
-                §  StructNet: Constructing and Mining Structure-Rich Information Networks for Scientific Research (NSF/IIS) <br/>
-                §  Taming Big Networks via Embedding (NSF/IIS-BIGDATA) <br/>
-                §  Mining and Leveraging Knowledge Hypercubes for Complex Applications (NSF-IIS) <br/>
-                §  NSF AI Institute for Molecular Discovery, Synthetic Strategy, and Manufacturing: Molecule Maker Lab Institute <br/>
-                §  DARPA/KAIROS: “RESIN: Reasoning about Event Schemas for Induction of kNowledge” <br/>
-                §  DARPA/INCAS: “Analytics of Information Influence: Effect Characterization” <br/>
+                L. Giaretta, A. Lekssays, B. Carminati, E. Ferrari, S. Girdzijauskas. LiMNet: Early-Stage Detection of IoT Botnets with Lightweight Memory Networks. ESORICS 2021: 605-625 <br/>
+                <br/>
+                Ha Xuan Son, Barbara Carminati, E. Ferrari. A Risk Assessment Mechanism for Android Apps. SmartIoT 2021: 237-244 <br/>
+                <br/>
+                P. Colombo, E. Ferrari, E. D. Tümer. Regulating data sharing across MQTT environments.J. Netw. Comput. Appl. 174: 102907 (2021)
                 </div>
             </div>
         </div>
