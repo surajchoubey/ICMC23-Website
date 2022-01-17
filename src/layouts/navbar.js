cfcfc0128387b2c75a4995f7c33311704f5cbd69
Suraj Choubey <srjchoubey2@gmail.com>
2022-01-17 22:31:19 +0530

    major changes in navbar

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -5,23 +5,26 @@ import { Link } from 'react-router-dom';
 function MyNavbar() {
 
 	return (
-    <div>
+    <div class="container-fluid">
 		<nav class="navbar navbar-light px-3 navbar-expand-lg sticky-top bg-white">
 			<div class="container-fluid">
+
 				<a class="navbar-brand my-2" href="/ICMC23-Website/">
-				<img src={NavLogo} height="70" class="d-inline-block align-top p-1" alt="" />
+				<img src={NavLogo} height="70" class="align-top p-1" alt="" />
 				</a>
+				
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
+
 				<div class='collapse navbar-collapse justify-content-end' id="navbarSupportedContent">
-					<ul class="navbar-nav ml-auto main-nav-list container b justify-content-end borderXwidth">
+					<ul class="navbar-nav main-nav-list justify-content-end borderXwidth">
 						<li class="nav-item">
 							<Link className='nav-link text-dark' data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/'>Home</Link>
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle text-dark" href='/ICMC23-Website/' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							About
+							About &nbsp;<i class="fa fa-angle-down" style={{color: 'black'}}></i>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdown" >
 								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/about/bpgc'>BPGC</Link>
@@ -37,28 +40,28 @@ function MyNavbar() {
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle text-dark" href='/ICMC23-Website/' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Committee
+							Committee &nbsp;<i class="fa fa-angle-down" style={{color: 'black'}}></i>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/committee/patrons/'>Patrons</Link>
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/committee/orgcommittee/'>Organizing Committee</Link>
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/committee/techprocommittee/'>Technical Program Committee</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/committee/patrons/'>Patrons</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/committee/orgcommittee/'>Organizing Committee</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/committee/techprocommittee/'>Technical Program Committee</Link>
 							</div>
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle text-dark" href='/ICMC23-Website/' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Call for Papers
+							Call for Papers &nbsp;<i class="fa fa-angle-down" style={{color: 'black'}}></i>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/tracks/'>Contribution Tracks</Link>
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/guidelines/'>Paper Guidelines</Link>
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/submitpaper/'>Submit your Paper</Link>
-							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/listaccepted/'>Accepted Papers</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/tracks/'>Contribution Tracks</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/guidelines/'>Paper Guidelines</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/submitpaper/'>Submit your Paper</Link>
+								<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/callforpapers/listaccepted/'>Accepted Papers</Link>
 							</div>
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle text-dark" href='/ICMC23-Website/' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Registration
+							Registration &nbsp;<i class="fa fa-angle-down" style={{color: 'black'}}></i>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/registration/confregfee/'>Conference Registration Fee</Link>
@@ -70,7 +73,7 @@ function MyNavbar() {
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle text-dark" href='/ICMC23-Website/' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Venue
+							Venue &nbsp;<i class="fa fa-angle-down" style={{color: 'black'}}></i>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 							<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to='/ICMC23-Website/venue/bgpc-goa'>BITS Goa</Link>
--- src/pages/home.js
+++ src/pages/home.js
@@ -11,10 +11,10 @@ import samplediv from '../images/speakers/samplediv.jpg';
 function Home() {
     return (
         <div>
-            <div class="container mb-3">
+            <div class="container">
                 <div class="row align-items-around">
                     <div class="col-sm-12 col-md-6">
-                        <div class="p-3" style={{minHeight: '500px', display: 'flex', flexDirection:'column', justifyContent: 'space-around', alignSelf: 'center'}}>
+                        <div class="p-4" style={{minHeight: '500px', display: 'flex', flexDirection:'column', justifyContent: 'space-around', alignSelf: 'center'}}>
                             <div class="d-flex">
                                 <h1 style={{textAlign: 'start', fontWeight:'lighter', color: 'grey'}}>
                                     9th International Conference on Mathematics and Computing
@@ -26,7 +26,7 @@ function Home() {
                                 </h1>
                             </div>
                             <div class="d-flex" style={{textAlign: 'start'}}>
-                                <div class="black-button">Registration</div>
+                                <button class="black-button">Registration</button>
                             </div>                                           
                             <div class="d-flex" style={{textAlign: 'start'}}>
                                 <h3 style={{fontWeight: 'lighter'}}>JANUARY 6-8 | 2023</h3>
@@ -34,7 +34,7 @@ function Home() {
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-6">
-                        <div class="p-5" style={{minHeight: '500px', display: 'flex', flexDirection:'column', justifyContent: 'space-between', alignItems: 'center'}}>
+                        <div class="p-3 justify-self-center" style={{minHeight: '500px', display: 'flex', flexDirection:'column', alignItems: 'center'}}>
                             <img src={BITSDome} alt="" class="img-fluid"/>
                         </div>
                     </div>
@@ -44,8 +44,8 @@ function Home() {
             <Timer />
 
             <div class="container my-5 py-5">
-                <div class="row">             
-                    <div class="col-lg-6 col-md-12 p-3" style={{minHeight:'350px', display: 'flex', flexDirection:'column', justifyContent: 'space-around'}}>
+                <div class="row align-middle">             
+                    <div class="col-lg-6 col-md-12 p-3" style={{minHeight:'350px'}}>
                         <div>
                             <h1 class="" style={{textAlign: 'start', fontWeight: 'lighter'}}>About<br/></h1>
                             <p class="" style={{textAlign: 'start'}}> Vellore Institute of Technology (VIT) was founded in 1984 as Vellore Engineering College by the Chancellor Dr. G. Viswanathan. VIT attracts students from all the 29 states of India and more than 41 different countries because of its academic excellence. The Government of India recognizes VIT as an Institution of Eminence (IoE). The credentials of VIT in academics and research have ranked VIT in 1st position among private institutions.</p>
@@ -56,10 +56,10 @@ function Home() {
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 d-block d-lg-block d-none d-sm-none" style={{minHeight:'350px'}}>
+                    <div class="col-lg-3 col-md-6 d-block d-lg-block d-none d-sm-none align-self-center">
                         <img src={BITSAudi} alt="" class="img-fluid"/>
                     </div>
-                    <div class="col-lg-3 col-md-6 d-block d-lg-block d-none d-sm-none" style={{minHeight:'350px'}}>
+                    <div class="col-lg-3 col-md-6 d-block d-lg-block d-none d-sm-none align-self-center">
                         <img src={BITSDClassroom} alt="" class="img-fluid"/>
                     </div>           
                 </div>
@@ -69,16 +69,16 @@ function Home() {
                 <div class="container">
                 <h1 style={{textAlign: 'start', fontWeight: 'lighter', color: 'gray'}} class="mb-5">Author's Guidelines</h1>
                     <div class="row justify-content-between">
-                        <div class="col-lg-1 col-md-2 col-sm-6">
-                            <div><h1 style={{fontSize: '600%', color:'gray'}}>1</h1></div>
+                        <div class="col-lg-1 col-md-2 col-sm-3">
+                            <div><h1 style={{fontSize: '500%', color:'gray', fontWeight: 'bold'}}>1</h1></div>
                         </div>
-                        <div class="col-lg-5 col-md-4 col-sm-6">
+                        <div class="col-lg-5 col-md-4 col-sm-9">
                             <div style={{textAlign: 'start'}}>Each paper must be original and unpublished work, not submitted for publication elsewhere (copyright infringement issues will be the responsibility of the authors). Papers must be written in good English. Accepted submissions may not appear in any other conference or workshop with proceedings.</div>
                         </div>
-                        <div class="col-lg-1 col-md-2 col-sm-6 mt-md-0 mt-sm-4">
-                            <div><h1 style={{fontSize: '600%', color:'gray'}}>2</h1></div>
+                        <div class="col-lg-1 col-md-2 col-sm-3 mt-md-0 mt-sm-4">
+                            <div><h1 style={{fontSize: '500%', color:'gray', fontWeight: 'bold'}}>2</h1></div>
                         </div>
-                        <div class="col-lg-5 col-md-4 col-sm-6 mt-md-0 mt-sm-4">
+                        <div class="col-lg-5 col-md-4 col-sm-9 mt-md-0 mt-sm-4">
                             <div style={{textAlign: 'start'}}>The submissions must be anonymous, i.e. without author name(s), affiliation(s), acknowledgments, or obvious references. It should begin with a title, a short abstract, and a list of keywords and its introduction should summarize the contributions of the paper</div>
                         </div>
                     </div>
@@ -97,7 +97,7 @@ function Home() {
                     <div class="col-3"><img src={whitenigga} alt="" class="img-fluid"/></div>
                 </div>
                 <div class="mt-5 d-flex flex-row-reverse">
-                    <div class="black-button">All Speakers</div>
+                    <a class="black-button">All Speakers</a>
                 </div>
             </div>
 
