98f3293b1e1b54beba85c3db596bd255c19776eb
YashT1304 <f20190834@goa.bits-pilani.ac.in>
2022-01-20 23:40:43 +0530

    fixed the issue

--- src/pages/callforpapers/listaccepted.js
+++ src/pages/callforpapers/listaccepted.js
@@ -1,52 +1,46 @@
 import React from "react";
-import {Helmet} from "react-helmet";
+import JsonData from './employee.json'
 
 function ListAccepted() {
-    return(
-        <div>
-            <Helmet>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-                <script>
-		    $(document).ready(function(){
-			$.getJSON("employee.json",function(data){
-			    var employee_data = '';
-			    $.each(data,function(key,value){
-				employee_data += '<tr>';
-				employee_data += '<td>'+value.Sl+'</td>';
-				employee_data += '<td>'+value.Id+'</td>';
-				employee_data += '<td>'+value.Title+'</td>';
-				employee_data += '<td>'+value.Authors+'</td>';
-				employee_data += '<td>'+value.Affiliation+'</td>';
-				employee_data += '<td>'+value.Country+'</td>';
-				employee_data += '</tr>';
+  const DisplayData=JsonData.map(
+    (info)=>{
+        return(
+            <tr>
+                <td>{info.Sl}</td>
+                <td>{info.Id}</td>
+                <td>{info.Title}</td>
+                <td>{info.Authors}</td>
+                <td>{info.Affiliation}</td>
+                <td>{info.Country}</td>
+            </tr>
+        )
+    }
+)
 
-			    });
-			    $('#employee_table').append(employee_data);
-			});
-		    });
-		</script>
-	    </Helmet>
-            <div class="container">
-            <div class="table-responsive section-header">
-                <h1 align = "center">
-                    Accepted Papers
-                </h1>
-                <br/>
+return(
+    <div class="Container">
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                <th>Sl</th>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Authors</th>
+                <th>Affiliation</th>
+                <th>Country</th>
+                </tr>
+            </thead>
+            <tbody>
+             
+
                 
-                <table class="table table-striped table-bordered" id="employee_table">
-                    <tr>
-                        <th>Sl</th>
-                        <th>Id</th>
-                        <th>Title</th>
-                        <th>Authors</th>
-                        <th>Affiliation</th>
-                        <th>Country</th>
-                    </tr>
-                </table>
-            </div>
-            </div>
-        </div>
-    );
-}
-
+                {DisplayData}
+                
+            </tbody>
+        </table>
+         
+    </div>
+)
+  
+} 
 export default ListAccepted;
