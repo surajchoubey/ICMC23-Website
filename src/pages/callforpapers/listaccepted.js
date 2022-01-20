0ca2f2d4cc09e1cdba749ccacb8ce20d4990b4a8
YashT1304 <f20190834@goa.bits-pilani.ac.in>
2022-01-20 17:08:07 +0530

    Update listaccepted.js

--- src/pages/callforpapers/listaccepted.js
+++ src/pages/callforpapers/listaccepted.js
@@ -5,27 +5,27 @@ function ListAccepted() {
     return(
         <div>
             <Helmet>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
                 <script>
-										$(document).ready(function(){
-												$.getJSON("employee.json",function(data){
-														var employee_data = '';
-														$.each(data,function(key,value){
-																employee_data += '<tr>';
-																employee_data += '<td>'+value.Sl+'</td>';
-																employee_data += '<td>'+value.Id+'</td>';
-																employee_data += '<td>'+value.Title+'</td>';
-																employee_data += '<td>'+value.Authors+'</td>';
-																employee_data += '<td>'+value.Affiliation+'</td>';
-																employee_data += '<td>'+value.Country+'</td>';
-																employee_data += '</tr>';
+    $(document).ready(function(){
+        $.getJSON("employee.json",function(data){
+            var employee_data = '';
+            $.each(data,function(key,value){
+                employee_data += '<tr>';
+                employee_data += '<td>'+value.Sl+'</td>';
+                employee_data += '<td>'+value.Id+'</td>';
+                employee_data += '<td>'+value.Title+'</td>';
+                employee_data += '<td>'+value.Authors+'</td>';
+                employee_data += '<td>'+value.Affiliation+'</td>';
+                employee_data += '<td>'+value.Country+'</td>';
+                employee_data += '</tr>';
 
-														});
-														$('#employee_table').append(employee_data);
-												});
-										});
-								</script>
-            </Helmet>
+            });
+            $('#employee_table').append(employee_data);
+        });
+    });
+</script>
+	    </Helmet>
             <div class="container">
             <div class="table-responsive section-header">
                 <h1 align = "center">
