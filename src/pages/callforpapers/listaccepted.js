08aff94a1336aa9938a0f541f5545d52ebea2aa0
YashT1304 <f20190834@goa.bits-pilani.ac.in>
2022-01-20 17:03:15 +0530

    Update listaccepted.js

--- src/pages/callforpapers/listaccepted.js
+++ src/pages/callforpapers/listaccepted.js
@@ -1,9 +1,50 @@
 import React from "react";
+import {Helmet} from "react-helmet";
 
 function ListAccepted() {
     return(
         <div>
-            This is the CallForPapers/ListAccepted Page.
+            <Helmet>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <script>
+										$(document).ready(function(){
+												$.getJSON("employee.json",function(data){
+														var employee_data = '';
+														$.each(data,function(key,value){
+																employee_data += '<tr>';
+																employee_data += '<td>'+value.Sl+'</td>';
+																employee_data += '<td>'+value.Id+'</td>';
+																employee_data += '<td>'+value.Title+'</td>';
+																employee_data += '<td>'+value.Authors+'</td>';
+																employee_data += '<td>'+value.Affiliation+'</td>';
+																employee_data += '<td>'+value.Country+'</td>';
+																employee_data += '</tr>';
+
+														});
+														$('#employee_table').append(employee_data);
+												});
+										});
+								</script>
+            </Helmet>
+            <div class="container">
+            <div class="table-responsive section-header">
+                <h1 align = "center">
+                    Accepted Papers
+                </h1>
+                <br/>
+                
+                <table class="table table-striped table-bordered" id="employee_table">
+                    <tr>
+                        <th>Sl</th>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Authors</th>
+                        <th>Affiliation</th>
+                        <th>Country</th>
+                    </tr>
+                </table>
+            </div>
+            </div>
         </div>
     );
 }
