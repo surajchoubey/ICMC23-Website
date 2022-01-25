f977dee2aff0f93b4f00e509087fd13015fff76f
Suraj Choubey <55239656+surajchoubey@users.noreply.github.com>
2022-01-25 10:06:36 +0530

    Revert "Yash t1304 patch 4"

--- src/pages/callforpapers/listaccepted.js
+++ src/pages/callforpapers/listaccepted.js
@@ -1,46 +1,11 @@
 import React from "react";
-import JsonData from './employee.json'
 
 function ListAccepted() {
-  const DisplayData=JsonData.map(
-    (info)=>{
-        return(
-            <tr>
-                <td>{info.Sl}</td>
-                <td>{info.Id}</td>
-                <td>{info.Title}</td>
-                <td>{info.Authors}</td>
-                <td>{info.Affiliation}</td>
-                <td>{info.Country}</td>
-            </tr>
-        )
-    }
-)
+    return(
+        <div>
+            This is the CallForPapers/ListAccepted Page.
+        </div>
+    );
+}
 
-return(
-    <div class="Container">
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                <th>Sl</th>
-                <th>Id</th>
-                <th>Title</th>
-                <th>Authors</th>
-                <th>Affiliation</th>
-                <th>Country</th>
-                </tr>
-            </thead>
-            <tbody>
-             
-
-                
-                {DisplayData}
-                
-            </tbody>
-        </table>
-         
-    </div>
-)
-  
-} 
 export default ListAccepted;
