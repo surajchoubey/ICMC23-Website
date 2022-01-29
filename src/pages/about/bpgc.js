83ec284657f178ac6a59a4f7226d20c2b89cff68
maybe-apex <utkarshomer6972@gmail.com>
2022-01-29 22:25:02 +0530

    fixed aboutBPGC page responsive

--- src/pages/about/bpgc.js
+++ src/pages/about/bpgc.js
@@ -1,68 +1,41 @@
 import React from "react";
-import abouttoppng from '../../images/assets/about-top.png';
-import aboutcontentpng from '../../images/assets/about-content.png';
-import aboutbottompng from '../../images/assets/about-bottom.png';
+import abouttoppng from "../../images/assets/about-top.png";
+import aboutcontentpng from "../../images/assets/about-content.png";
+import aboutbottompng from "../../images/assets/about-bottom.png";
+
+const aboutText =
+  " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta vitaelaboriosam sequi accusamus nobis delectus omnis nesciunt temporibusnihil necessitatibus illo dicta quos, natus aliquam ad! Praesentiumconsequatur iste fugiat obcaecati animi minus magnam tempora numquamdistinctio suscipit accusantium deserunt, cumque minima mollitia fugitea eaque. Vero labore necessitatibus nisi veniam temporibus,consequatur, cumque provident quisquam in eum cupiditate nobis illum,dolor perspiciatis laudantium similique magni repudiandae ipsam? Porrosequi tempore doloribus amet, officiis iusto quis similique dolores,cumque ipsum, fuga quas recusandae nam saepe obcaecati perspiciatis.Ullam hic voluptates veritatis voluptate eum esse, harum numquamdistinctio quod necessitatibus cumque quisquam, ducimus consecteturnemo vel similique unde ex est illum mollitia aspernatur commodivoluptatum cupiditate? Cum blanditiis, alias minus, reiciendisarchitecto reprehenderit facilis quam, odit quae inventore iure itaquelaboriosam rerum quidem repellendus eum incidunt harum magni deseruntcommodi nobis. Voluptate laudantium pariatur debitis ut quibusdam oditquas dolor aspernatur vitae temporibus accusamus animi facilis, quosnihil fugit consequatur qui omnis accusantium delectus doloremquesuscipit id molestias? Recusandae facilis sint enim inventore quasblanditiis amet id possimus at iure dolorem, saepe cupiditate.Facilis, eum quia quidem officia recusandae sint. Nisi libero illo,officia eum illum labore enim possimus ab pariatur corporis laudantiumeius dignissimos autem sed expedita est qui mollitia unde obcaecati?Dolorum et hic numquam veniam dolore voluptas beatae ";
 
 function BPGC() {
-    return(
-        <section class="section-about">
-        <div className="titleStyle1">
-          About the Campus
-        </div>
-        <div className="titleStyle2">
-		Birla Institute of Technology and Science
-        </div>
-        <div class="divider"></div>
-        <div class="about-content">
-          <img
-            src={abouttoppng}
-            alt="Professors and Students attending a conference"
-            class="about-img-top"
-          />
+  return (
+    <section class="section-about">
+      <div className="titleStyle1">About the Campus</div>
+      <div className="titleStyle2">
+        Birla Institute of Technology and Science
+      </div>
+      <div class="divider"></div>
+      <div class="about-content">
+        <img
+          src={abouttoppng}
+          alt="Professors and Students attending a conference"
+          class="about-img-top"
+        />
+        <div className="about-mid-grid">
           <img
             src={aboutcontentpng}
             alt="Speaker at a conference"
             class="about-img-cnt"
           />
-          <p class="about-txt-cnt">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta vitae
-            laboriosam sequi accusamus nobis delectus omnis nesciunt temporibus
-            nihil necessitatibus illo dicta quos, natus aliquam ad! Praesentium
-            consequatur iste fugiat obcaecati animi minus magnam tempora numquam
-            distinctio suscipit accusantium deserunt, cumque minima mollitia fugit
-            ea eaque. Vero labore necessitatibus nisi veniam temporibus,
-            consequatur, cumque provident quisquam in eum cupiditate nobis illum,
-            dolor perspiciatis laudantium similique magni repudiandae ipsam? Porro
-            sequi tempore doloribus amet, officiis iusto quis similique dolores,
-            cumque ipsum, fuga quas recusandae nam saepe obcaecati perspiciatis.
-            Ullam hic voluptates veritatis voluptate eum esse, harum numquam
-            distinctio quod necessitatibus cumque quisquam, ducimus consectetur
-            nemo vel similique unde ex est illum mollitia aspernatur commodi
-            voluptatum cupiditate? Cum blanditiis, alias minus, reiciendis
-            architecto reprehenderit facilis quam, odit quae inventore iure itaque
-            laboriosam rerum quidem repellendus eum incidunt harum magni deserunt
-            commodi nobis. Voluptate laudantium pariatur debitis ut quibusdam odit
-            quas dolor aspernatur vitae temporibus accusamus animi facilis, quos
-            nihil fugit consequatur qui omnis accusantium delectus doloremque
-            suscipit id molestias? Recusandae facilis sint enim inventore quas
-            blanditiis amet id possimus at iure dolorem, saepe cupiditate.
-            Facilis, eum quia quidem officia recusandae sint. Nisi libero illo,
-            officia eum illum labore enim possimus ab pariatur corporis laudantium
-            eius dignissimos autem sed expedita est qui mollitia unde obcaecati?
-            Dolorum et hic numquam veniam dolore voluptas beatae cumque alias
-            saepe recusandae soluta veritatis ad animi fuga deserunt libero
-            reiciendis, laboriosam error velit culpa magni. Incidunt obcaecati
-            fuga voluptas est, quod debitis! Ipsam reprehenderit deleniti maxime
-            quod facilis?
-          </p>
-          <img
-            src={aboutbottompng}
-            alt="Scenic Campus View"
-            class="about-img-btm"
-          />
+          <p class="about-txt-cnt">{aboutText}</p>
         </div>
-      </section>
-    );
+        <img
+          src={aboutbottompng}
+          alt="Scenic Campus View"
+          class="about-img-btm"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default BPGC;
