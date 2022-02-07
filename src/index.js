4ecbb4577bff0472cc6d65f3e51ccb5598e4a3d7
Suraj Choubey <srjchoubey2@gmail.com>
2022-02-07 09:02:03 +0530

    added forceRefresh

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter forceRefresh={true}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
