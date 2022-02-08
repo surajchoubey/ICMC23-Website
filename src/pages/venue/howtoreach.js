3f7663d622ed5cd53a7f798c1b24c8bc40cb92e6
YashT1304 <f20190834@goa.bits-pilani.ac.in>
2022-02-08 20:24:49 +0530

    Update howtoreach.js

--- src/pages/venue/howtoreach.js
+++ src/pages/venue/howtoreach.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import "./styles.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
-
+import {BITSGoaOnGMAP} from "../../layouts/bits-goa-map.js";
 import BTable from "react-bootstrap/Table";
 
 function Howtoreach() {
@@ -46,53 +46,19 @@ function Howtoreach() {
         <p></p>
         <p>
           <strong>
-            For more details, Visit:{" "}
-            <a href="https://www.bits-pilani.ac.in/goa/reachGoaCampus">
-              How to Reach BITS Pilani, Goa Campus
+            For more details, visit:{" "}
+            <a href="https://www.bits-pilani.ac.in/goa/reachGoaCampus" rel="noopener noreferer">
+              How to Reach BITS Pilani Goa Campus?
             </a>
           </strong>
         </p>
       </div>
 
       <div>
-        <section class="map cid-ryayIXJ2Pr" id="map1-i">
-          <div
-            class="mbr-row mbr-justify-content-center"
-            style={{ display: "flex", alignItems: "center" }}
-          >
-            <div class="map-title mbr-col-sm-12 mbr-col-md-12 align-center"></div>
-            <div class="google-map">
-              <amp-iframe
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEIpgj38KyLFELm2bK9Y7krBkz1K-cMq8&amp;q=place_id:ChIJbWZRWDe4vzsRQAIREDBo1Uo"
-                height="400"
-                layout="fill"
-                sandbox="allow-scripts allow-same-origin allow-popups"
-                frameborder="0"
-                class="mobirise-loader i-amphtml-element i-amphtml-layout-fill i-amphtml-layout-size-defined i-amphtml-built i-amphtml-layout"
-                i-amphtml-layout="fill"
-              >
-                <div placeholder="" class="placeholder amp-hidden">
-                  <div class="mobirise-spinner">
-                    <em></em>
-                    <em></em>
-                    <em></em>
-                  </div>
-                </div>
-                <i-amphtml-scroll-container class="amp-active">
-                  <iframe
-                    title="bits"
-                    class="i-amphtml-fill-content"
-                    name="amp_iframe0"
-                    frameborder="0"
-                    allow=""
-                    sandbox="allow-scripts allow-same-origin allow-popups"
-                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEIpgj38KyLFELm2bK9Y7krBkz1K-cMq8&amp;q=place_id:ChIJbWZRWDe4vzsRQAIREDBo1Uo#amp=1"
-                  ></iframe>
-                </i-amphtml-scroll-container>
-              </amp-iframe>
-            </div>
-          </div>
-        </section>
+
+        <BITSGoaOnGMAP height={"400px"} width={"100%"} />
+
+        <p></p>
       </div>
     </div>
   );
