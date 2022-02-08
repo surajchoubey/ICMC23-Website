121b6319e7b74defcace64a34b184c7a58f204e9
YashT1304 <f20190834@goa.bits-pilani.ac.in>
2022-02-08 20:27:50 +0530

    Update visa.js

--- src/pages/venue/visa.js
+++ src/pages/venue/visa.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {ExcuseCard, Excuses} from "../../layout/excuses.js"
 
 function VISA() {
     return(
         <div className="container">
+
             <div className="titleStyle1">VISA</div>
             <div className="titleStyle2">Information</div>
             <div class="divider" style={{marginBottom: '30px'}}></div>
+            {ExcuseCard(Excuses.TBA)}
+            {ExcuseCard(Excuses.UnderDevelopmentPhase)}
+
+
         </div>
     );
 }
