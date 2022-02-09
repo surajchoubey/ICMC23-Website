72423c53e80266ed4dc34bc2e3fa83013a527458
Suraj Choubey <srjchoubey2@gmail.com>
2022-02-09 08:00:35 +0530

    navbar fixes unnoticeable

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -25,7 +25,7 @@ let NavWithoutDropDown = ({link, showname}) => {
 
 let DropDownItem = ({link, showname}) => {
 	return (
-		<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to={link}>{showname}</Link>
+		<Link class="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show" to={link}> <center>{showname}</center> </Link>
 	)
 }
 
@@ -44,7 +44,7 @@ function MyNavbar() {
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
-				<div class='collapse navbar-collapse justify-content-end' id="navbarSupportedContent">
+				<div class='collapse navbar-collapse in justify-content-end' id="navbarSupportedContent">
 					<ul class="navbar-nav d-flex flex-wrap main-nav-list justify-content-end borderXwidth">
 						<NavWithoutDropDown link={"/ICMC23-Website/"} showname={"Home"} />
 
