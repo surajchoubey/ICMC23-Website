c16f9328b7c24e33397815a4d7f920deb5054b13
Suraj Choubey <srjchoubey2@gmail.com>
2022-02-17 13:11:41 +0530

    submit paper portal uncommented

--- src/pages/callforpapers/submitpaper.js
+++ src/pages/callforpapers/submitpaper.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { ExcuseCard, Excuses } from "../../layouts/excuses.js";
+
+const conf_link = "https://easychair.org/conferences/?conf=icmc2023";
+const paragraph = "The authors are pleasingly invited to submit the full paper of their original, unpublished, research contribution which is not currently under review by another conference or journal. Only the accepted and registered papers will be allowed to present at the conference.";
+const paragraph_bold_line = "The paper must be submitted using the EasyChair portal.";
+const note = "Accepted and registered papers will be published by a Springer series (proposal submitted).";
 
 function SubmitPaper() {
   return (
@@ -9,36 +13,30 @@ function SubmitPaper() {
           <div className="titleStyle1"> Submit Your Paper </div>
           <div className="titleStyle2"> Information </div>
           <div class="divider" style={{ marginBottom: "30px" }}></div>
-          {/* <p class="" align="justify">
-            The authors are pleasingly invited to submit the full paper of their
-            original, unpublished, research contribution which is not currently
-            under review by another conference or journal. Only the accepted and
-            registered papers will be allowed to present at the conference.
-          </p> */}
+          <p class="" align="justify"> {paragraph} <b> {paragraph_bold_line} </b> </p>
         </div>
 
-        {/* <div class="col-lg- cta-btn-container text-center">
+        <div class="col-lg- cta-btn-container text-center">
           <p class="cta-text " align="center">
-            Click on the Submit button for paper submission
             <br />
             <a
               class="cta-btn black-button"
-              href=" https://easychair.org/conferences/?conf=icmc2022"
+              rel="nopener noreferrer"
+              href={conf_link}
               target="_blank"
             >
-              Submit
+              link to easychair portal for icmc 2023
             </a>
           </p>
-        </div> */}
-        {/* <div class="text-center text-lg-left">
-          <p class="cta-text">
-            <b>
-              All accepted and registered papers will be published by a Springer
-              series (proposal submitted).
-            </b>
+        </div>
+        <div class="text-center text-lg-left">
+          <br/>
+          <p class="cta-text justify">
+            <b> Note: </b> 
+              {note}
           </p>
-        </div> */}
-        {ExcuseCard(Excuses.TBU)}
+        </div>
+        {/* ExcuseCard(Excuses.TBU) */} 
       </div>
     </div>
   );
