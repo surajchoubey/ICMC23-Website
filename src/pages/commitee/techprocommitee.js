2b894e5b46fefd3fe22182275ffd73cc6a07e8f5
YashT1304 <f20190834@goa.bits-pilani.ac.in>
2022-03-28 18:57:14 +0530

    Update techprocommitee.js

--- src/pages/commitee/techprocommitee.js
+++ src/pages/commitee/techprocommitee.js
@@ -1,24 +1,26 @@
 import React from "react";
-// import JSONData from "./techcomm.json";
-// import BTable from "react-bootstrap/Table";
-import { ExcuseCard, Excuses } from "../../layouts/excuses.js";
+import JSONData from "./techcomm.json";
+import BTable from "react-bootstrap/Table";
+// import "./styles.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { ExcuseCard, Excuses } from "../../layouts/excuses.js";
 
 function TechProCommittee() {
-  // const DisplayData = JSONData.map((info) => {
-  //   return (
-  //     <tr>
-  //       <td>{info.Name}</td>
-  //       <td>{info.Organization}</td>
-  //     </tr>
-  //   );
-  // });
+  const DisplayData = JSONData.map((info) => {
+    return (
+      <tr>
+        <td>{info.Name}</td>
+        <td>{info.Institution}</td>
+      </tr>
+    );
+  });
 
   return (
     <>
       <div className="titleStyle1"> Technical Program </div>
       <div className="titleStyle2"> Committee </div>
       <div class="divider" style={{ marginBottom: "30px" }}></div>
-      {/* <BTable striped bordered hover responsive>
+      <BTable striped bordered hover responsive>
         <thead>
           <tr>
             <th>Name</th>
@@ -26,10 +28,10 @@ function TechProCommittee() {
           </tr>
         </thead>
         <tbody>{DisplayData}</tbody>
-      </BTable> */}
+      </BTable>
       {/* {TBA("test")} */}
-      {ExcuseCard(Excuses.TBA)}
-      <br/>
+      {/* {ExcuseCard(Excuses.TBA)} */}
+      <br />
     </>
   );
 }
