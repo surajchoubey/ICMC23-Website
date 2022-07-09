e85b56db755134eb3b355610cd090728ecd041c4
Suraj Choubey <srjchoubey2@gmail.com>
2022-07-09 12:17:08 +0530

    removed hyperlink

--- src/pages/about/icmc.js
+++ src/pages/about/icmc.js
@@ -4,8 +4,7 @@ import icmclogo from "../about/icmc-logo.jpg"
 const aboutText = "The conference started its journey from 2013 at Haldia Institute of Technology, India. The flag of the conference was carried by different renowned institutes like IIT (BHU), KIIT, Bhubaneswar, Sikkim University, India etc."
 const aboutText3 = "ICMC 2023 aims to bring together both novice and experienced scientists with developers, to meet new colleagues, collect new ideas and establish new cooperation between research groups and provide a platform for researchers from academic and industry to present their original work and exchange ideas, information, techniques and applications in the field of Computational Applied Mathematics, including, but not limited to the broad topics of Operations Research, Numerical Analysis, Computational Fluid Mechanics, Soft Computing, Cryptology & Security Analysis, Image Processing, Big Data, Cloud Computing, Data Analytics, IoT, Pervasive Computing and other emerging areas of research.";
 
-const aboutText2 = "The conference will be conducted in hybrid mode";
-const aboutText2_2 = " but more preferable for offline mode. In the pandemic situation, travel has become difficult, so those who are unable to travel can present via the online platform google meet.";
+const aboutText2 = "The conference will be conducted in hybrid mode but more preferable for offline mode. In the pandemic situation, travel has become difficult, so those who are unable to travel can present via the online platform google meet.";
 
 function ICMC() {
     return(
@@ -23,10 +22,7 @@ function ICMC() {
             className="about-img-logo"
           />
           <p style={{fontWeight: '400'}} className="about-txt-cnt">{aboutText}</p>
-          <p style={{fontWeight: '400'}} className="about-txt-cnt">
-            <mark style={{ backgroundColor: 'yellow'}}>{aboutText2}</mark> 
-            {aboutText2_2}
-          </p>
+          <p style={{fontWeight: '400'}} className="about-txt-cnt">{aboutText2}</p>
           <p style={{fontWeight: '400'}} className="about-txt-cnt">{aboutText3}</p>
         </div>
       </div>
