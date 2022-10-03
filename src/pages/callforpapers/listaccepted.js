024348eaa5b8998fa74332d698d623bf35350c8e
Yash Trivedi <f20190834@goa.bits-pilani.ac.in>
2022-10-04 00:03:42 +0530

    Update listaccepted.js

--- src/pages/callforpapers/listaccepted.js
+++ src/pages/callforpapers/listaccepted.js
@@ -1,44 +1,37 @@
 import React from "react";
 // import JSONdata from "./employee.json";
-import { ExcuseCard, Excuses } from "../../layouts/excuses.js";
+import JSONData from "./acceptedpapers.json";
+// import { ExcuseCard, Excuses } from "../../layouts/excuses.js";
+import BTable from "react-bootstrap/Table";
 
-function ListAccepted() {
-  // const DisplayData = JSONdata.map((info) => {
-  //   return (
-  //     <tr>
-  //       <td>{info.S_No}</td>
-  //       <td>{info.ID}</td>
-  //       <td>{info.Title}</td>
-  //       <td>{info.Authors}</td>
-  //       <td>{info.Affiliation}</td>
-  //       <td>{info.Country}</td>
-  //     </tr>
-  //   );
-  // },
-  // );
+function AcceptedPapers() {
+  const DisplayData = JSONData.map((info) => {
+    return (
+      <tr>
+        <td>{info.AUTHORS}</td>
+        <td>{info.TITLE}</td>
+      </tr>
+    );
+  });
 
   return (
-    <div class="container">
-      <div className="titleStyle1"> List Of </div>
+    <>
+      <div className="titleStyle1"> List of </div>
       <div className="titleStyle2"> Accepted Papers </div>
       <div class="divider" style={{ marginBottom: "30px" }}></div>
-      {/* <table className="table table-striped">
-            <thead>
-                <tr>
-                <th>S No.</th>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Authors</th>
-                <th>Affiliation</th>
-                <th>Country</th>
-                </tr>
-            </thead>
-            <tbody>
-                {DisplayData}
-            </tbody>
-        </table> */}
-      {ExcuseCard(Excuses.TBA)}
-    </div>
+      <BTable striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Authors</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{DisplayData}</tbody>
+      </BTable>
+      {/* {TBA("test")} */}
+      {/* {ExcuseCard(Excuses.TBA)} */}
+      <br />
+    </>
   );
 }
-export default ListAccepted;
+export default AcceptedPapers;
