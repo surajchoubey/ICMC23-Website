4a4d13b6fdf35ed29e2b341515088a72503d004d
Yash Trivedi <f20190834@goa.bits-pilani.ac.in>
2022-10-12 13:33:26 +0530

    listaccepted
    back to previous update. hide table and pop up excuse card

--- src/pages/callforpapers/listaccepted.js
+++ src/pages/callforpapers/listaccepted.js
@@ -1,31 +1,33 @@
 import React from "react";
 // import JSONdata from "./employee.json";
-import { acceptedList} from "./acceptedpapers";
-// import { ExcuseCard, Excuses } from "../../layouts/excuses.js";
+// import { acceptedList } from "./acceptedpapers";
+import { ExcuseCard, Excuses } from "./excuses.js";
 import BTable from "react-bootstrap/Table";
+// import "./styles.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-function sentenceCapitalize(s) {
-  return s.toLowerCase().replace( /\b./g, (word) => {
-    return word.toUpperCase(); 
-  });
-};
+// function sentenceCapitalize(s) {
+//   return s.toLowerCase().replace( /\b./g, (word) => {
+//     return word.toUpperCase();
+//   });
+// };
 
 function AcceptedPapers() {
-  const DisplayData = acceptedList.map((info) => {
-    return (
-      <tr>
-        <td style={{ textAlign: 'left' }}> { sentenceCapitalize(info.AUTHORS) } </td>
-        <td style={{ textAlign: 'left' }}> { sentenceCapitalize(info.TITLE) } </td>
-      </tr>
-    );
-  });
+  // // const DisplayData = acceptedList.map((info) => {
+  //   return (
+  //     <tr>
+  //       <td style={{ textAlign: 'left' }}> { sentenceCapitalize(info.AUTHORS) } </td>
+  //       <td style={{ textAlign: 'left' }}> { sentenceCapitalize(info.TITLE) } </td>
+  //     </tr>
+  //   );
+  // });
 
   return (
     <div className="container">
       <div className="titleStyle1"> List of </div>
       <div className="titleStyle2"> Accepted Papers </div>
       <div class="divider" style={{ marginBottom: "30px" }}></div>
-      <h3 style={{ textAlign: 'left' }} > ACCEPTED ABSTRACT PAPERS FOR ORAL PRESENTATION </h3>
+      {/* <h3 style={{ textAlign: 'left' }} > ACCEPTED ABSTRACT PAPERS FOR ORAL PRESENTATION </h3>
       <BTable striped bordered hover responsive>
         <thead>
           <tr>
@@ -34,8 +36,9 @@ function AcceptedPapers() {
           </tr>
         </thead>
         <tbody>{DisplayData}</tbody>
-      </BTable>
-      <br />
+      </BTable> */}
+      {/* <br /> */}
+      {ExcuseCard(Excuses.TBA)}
     </div>
   );
 }
